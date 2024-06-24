5cdff665d3a79734d2b0bd8b9c4eaf979b9d4669
Thoai1503 <vothoai1503@gmail.com>
2024-06-24 16:42:54 +0100

    first commit

--- src/Alternative.js
+++ src/Alternative.js
@@ -1,24 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = React.useState(0);
 
-  useEffect(() => {
-    const lastIndex = people.length - 1;
-    if (index < 0) {
-      setIndex(lastIndex);
-    }
-    if (index > lastIndex) {
-      setIndex(0);
-    }
-  }, [index, people]);
+  const nextSlide = () => {
+    setIndex((oldIndex) => {
+      let index = oldIndex + 1;
+      if (index > people.length - 1) {
+        index = 0;
+      }
+      return index;
+    });
+  };
+  const prevSlide = () => {
+    setIndex((oldIndex) => {
+      let index = oldIndex - 1;
+      if (index < 0) {
+        index = people.length - 1;
+      }
+      return index;
+    });
+  };
+
+  // useEffect(() => {
+  //   const lastIndex = people.length - 1
+  //   if (index < 0) {
+  //     setIndex(lastIndex)
+  //   }
+  //   if (index > lastIndex) {
+  //     setIndex(0)
+  //   }
+  // }, [index, people])
 
   useEffect(() => {
     let slider = setInterval(() => {
-      setIndex(index + 1);
+      setIndex((oldIndex) => {
+        let index = oldIndex + 1;
+        if (index > people.length - 1) {
+          index = 0;
+        }
+        return index;
+      });
     }, 5000);
     return () => {
       clearInterval(slider);
@@ -36,15 +61,15 @@ function App() {
         {people.map((person, personIndex) => {
           const { id, image, name, title, quote } = person;
 
-          let position = 'nextSlide';
+          let position = "nextSlide";
           if (personIndex === index) {
-            position = 'activeSlide';
+            position = "activeSlide";
           }
           if (
             personIndex === index - 1 ||
             (index === 0 && personIndex === people.length - 1)
           ) {
-            position = 'lastSlide';
+            position = "lastSlide";
           }
 
           return (
@@ -57,10 +82,10 @@ function App() {
             </article>
           );
         })}
-        <button className="prev" onClick={() => setIndex(index - 1)}>
+        <button className="prev" onClick={prevSlide}>
           <FiChevronLeft />
         </button>
-        <button className="next" onClick={() => setIndex(index + 1)}>
+        <button className="next" onClick={nextSlide}>
           <FiChevronRight />
         </button>
       </div>
